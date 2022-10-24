d4e3dec67538401ac253cb7a94724bba6134a854
Mahabubul Hasan <73310789+MahabubulHasanAkash@users.noreply.github.com>
2022-10-24 13:36:49 +0600

    refactor: changed shurjopay config,plugin,test feat: added readme

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="shurjopay",
+    version="0.0.3",
+    author="Mahabubul Hasan",
+
+    author_email="mahabubul@pranisheba.com",
+    description="shurjopay version 3 payment gateway integration package for python users.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shurjopay-plugins/sp-plugin-python.git",
+    project_urls={
+        "Bug Tracker": "https://github.com/shurjopay-plugins/sp-plugin-python.git",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['requests', 'django-environ'],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+)
