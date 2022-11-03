34561e08dd884b2172995afd8fc3dedfd611f525
mahabubul470 <mahabubul470@gmail.com>
2022-11-03 15:28:46 +0600

    refactor: changed setuptools, edited readme

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['requests', 'django-environ'],
+    install_requires=['requests'],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.7",
