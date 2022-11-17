39307d7a76fe8cd862e098d3ae91af1fd4677542
mahabubul470 <mahabubul470@gmail.com>
2022-11-17 16:01:50 +0600

    added dynamic log location

--- src/shurjopay_plugin/logger_config.py
+++ src/shurjopay_plugin/logger_config.py
@@ -1,14 +1,31 @@
 import os
 import logging
+from enum import Enum
+
+
+
+def get_log_dir():
+    #TODO test the log locations in windows and linux
+    """
+    Get the log directory based on different operating systems
+    """
+    log_dir = ""
+    if os.name == 'nt':
+        log_dir = os.path.expandvars(r'%APPDATA%\Local')
+    else:
+        log_dir = os.path.expanduser('~/var/')
+    return log_dir
+
 
 '''
     Logger configurations for Shurjopay
     Creates a logger instance to log the actions ,Defines file handeler and set formatter,Adds file handler to logger
 '''
-basedir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 logger = logging.getLogger(__name__)
 logger.info("ShurjopayConfig initialized")
-log_fileName = os.path.join(basedir, 'logs')+'/shurjopay.log'
+
+log_fileName = os.path.join(get_log_dir(), 'logs')+'/shurjopay.log'
 os.makedirs(os.path.dirname(log_fileName), exist_ok=True)
 logging.basicConfig(
     level=logging.ERROR,
@@ -18,3 +35,4 @@ logging.basicConfig(
         logging.StreamHandler()
     ]
 )
+
