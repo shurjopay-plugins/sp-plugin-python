34ec5bb7aaa4742d78c8a8976d19df42e71dc562
mahabubul470 <mahabubul470@gmail.com>
2022-11-24 16:27:45 +0600

    refactor: add exception handling for getIp method

--- src/shurjopay_plugin/netwotk_interface.py
+++ src/shurjopay_plugin/netwotk_interface.py
@@ -1,9 +1,14 @@
 from urllib.request import urlopen
+from shurjopay_exceptions import ShurjoPayException
 import re as r
+
 def getIP():
-    '''Returns the IP address of the marchents server'''
-    d = str(urlopen('http://checkip.dyndns.com/')
-            .read())
-
-    return r.compile(r'Address: (\d+\.\d+\.\d+\.\d+)').search(d).group(1)
-
+    '''
+    @return the IP address of the marchents server
+    @raise ShurjoPayException if IP address is not found 
+    '''
+    try:
+        data = str(urlopen('http://checkip.dyndns.com/').read()) # Open the oracle checkip url and read the data
+        return r.compile(r'Address: (\d+\.\d+\.\d+\.\d+)').search(data).group(1) # Return the IP address by using regex
+    except ShurjoPayException as ex:
+        raise ShurjoPayException('Marchent IP Address not found!', ex) # Raise exception if IP address is not found
