1c4777aa99c235d46fb4523f24c66c8407103f28
mahabubul470 <mahabubul470@gmail.com>
2022-11-24 16:26:49 +0600

    feat: add log directory to the logging configuration

--- src/shurjopay_plugin/logger_config.py
+++ src/shurjopay_plugin/logger_config.py
@@ -1,38 +1,32 @@
 import os
 import logging
-from enum import Enum
-
-
-
-def get_log_dir():
-    #TODO test the log locations in windows and linux
-    """
-    Get the log directory based on different operating systems
-    """
-    log_dir = ""
-    if os.name == 'nt':
-        log_dir = os.path.expandvars(r'%APPDATA%\Local')
-    else:
-        log_dir = os.path.expanduser('~/var/')
-    return log_dir
-
-
-'''
+class ShurjopayLoggerConfig(object):
+    '''
     Logger configurations for Shurjopay
-    Creates a logger instance to log the actions ,Defines file handeler and set formatter,Adds file handler to logger
-'''
-
-logger = logging.getLogger(__name__)
-logger.info("ShurjopayConfig initialized")
-
-log_fileName = os.path.join(get_log_dir(), 'logs')+'/shurjopay.log'
-os.makedirs(os.path.dirname(log_fileName), exist_ok=True)
-logging.basicConfig(
-    level=logging.ERROR,
-    format=" '%(asctime)s : %(levelname)s :gs %(name)s : %(funcName)s  %(message)s'",
-    handlers=[
-        logging.FileHandler(log_fileName, mode="a", encoding=None),
-        logging.StreamHandler()
-    ]
-)
+    Creates a logger instance to log the actions, 
+    Defines file handeler and set formatter,
+    Adds file handler to logger
+    '''
+    def __init__(self, log_file_path):
+        '''
+        @param log_file_path: path to log file
+        '''
+        self.log_file_path = log_file_path
+       
+    def get_logger(self):
+        '''
+        @return: logger instance
+        '''
+        logger = logging.getLogger(__name__)
+        logger.info("ShurjopayConfig initialized")
+        os.makedirs(os.path.dirname(self.log_file_path), exist_ok=True) # create log file if not exists
+        logging.basicConfig( # set basic config
+            level=logging.INFO,
+            format=" '%(asctime)s : %(levelname)s :gs %(name)s : %(funcName)s  %(message)s'",
+            handlers=[
+                logging.FileHandler(self.log_file_path, mode="a", encoding=None,delay=True),
+                logging.StreamHandler()
+            ]
+        )
+        return logger
 
