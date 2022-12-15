035e9e5a763a640d121ec1786981f4ea474c0050
mahabubul470 <mahabubul470@gmail.com>
2022-12-15 13:23:45 +0600

    Add python doc-string

--- src/shurjopay_plugin/netwotk_interface.py
+++ src/shurjopay_plugin/netwotk_interface.py
@@ -1,14 +1,15 @@
 from urllib.request import urlopen
-from .shurjopay_exceptions import ShurjoPayException
+from .shurjopay_exceptions import ShurjopayException
 import re as r
 
 def getIP():
     '''
-    :return the IP address of the marchents server
+    This method is used to get the IP address of the marchent server
+    :return the IP address as string
     :raise ShurjoPayException if IP address is not found 
     '''
     try:
         data = str(urlopen('http://checkip.dyndns.com/').read()) # Open the oracle checkip url and read the data
         return r.compile(r'Address: (\d+\.\d+\.\d+\.\d+)').search(data).group(1) # Return the IP address by using regex
-    except ShurjoPayException as ex:
-        raise ShurjoPayException('Marchent IP Address not found!', ex) # Raise exception if IP address is not found
+    except ShurjopayException as ex:
+        raise ShurjopayException('Marchent IP Address not found!', ex) # Raise exception if IP address is not found
