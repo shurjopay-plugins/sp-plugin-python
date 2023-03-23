739252056baff1e911e52909cb70ea1ace102fe9
Mahabubul Hasan <mahabubul470@gmail.com>
2023-03-23 15:32:39 +0600

    Merge live integration bug fix branch to dev  (#8)
    * Misc fixes: log permission, live model, Remove payment status check method

--- src/shurjopay_plugin/logger_config.py
+++ src/shurjopay_plugin/logger_config.py
@@ -14,7 +14,7 @@ class ShurjopayLoggerConfig(object):
         """Returns: default logger instance."""
         return self.logger
     
-    def get_file_logger(self,file_path):
+    def get_file_logger(self,log_file,permissions=0o644):
         """ Creates a file logger (shurjopay.log) at the file path provided.
        Log level is set to INFO by default. 
        The log file is created if it does not exist 
@@ -26,14 +26,18 @@ class ShurjopayLoggerConfig(object):
         Returns:
             logger: file logger instance.
         """
-        # create log file if it does not exist
-        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        # Create the log file if it doesn't exist
+        if not os.path.exists(log_file):
+            with open(log_file, 'w') as f:
+                f.write('Shurjopay Log file Generated\n')
+        # Set the permissions on the log file
+        os.chmod(log_file, permissions)
         # set basic configuration for logger
         logging.basicConfig(  
             level=self.loglevel,
             format=" '%(asctime)s : %(levelname)s :gs %(name)s : %(funcName)s  %(message)s'",
             handlers=[
-                logging.FileHandler(file_path,
+                logging.FileHandler(log_file,
                                     mode="a", encoding=None, delay=True),
                 logging.StreamHandler()
             ]
--- src/shurjopay_plugin/models.py
+++ src/shurjopay_plugin/models.py
@@ -24,34 +24,32 @@ class ShurjoPayTokenModel(object):
 class PaymentRequestModel(object):
     '''This class is used to store payment request details'''
     def __init__(self, amount, order_id,  currency, customer_name, customer_address, customer_phone, customer_city, customer_post_code) -> None:
-        #self.prefix:str = prefix # payment prefix
-        self.amount:str = amount # request amount
-        self.order_id:str = order_id # marchent order id
-        self.currency:str = currency # payment currency
-        self.customer_name:str = customer_name # customer name
-        self.customer_address:str = customer_address # customer address
-        self.customer_phone:str = customer_phone # customer phone
-        self.customer_city:str = customer_city # customer city 
-        self.customer_post_code:str = customer_post_code # customer post code
+        self.amount:str = amount
+        self.order_id:str = order_id 
+        self.currency:str = currency 
+        self.customer_name:str = customer_name 
+        self.customer_address:str = customer_address 
+        self.customer_phone:str = customer_phone
+        self.customer_city:str = customer_city 
+        self.customer_post_code:str = customer_post_code 
 
 
 class PaymentDetailsModel(object):
     '''This class is used to store payment details'''
-    def __init__(self, checkout_url, amount, currency, sp_order_id, customer_order_id,sp_code, customer_name, customer_address, customer_city, customer_phone, customer_email, client_ip, intent, transactionStatus) -> None:
+    def __init__(self, checkout_url, amount, currency, sp_order_id, customer_order_id, customer_name, customer_address, customer_city, customer_phone, customer_email, client_ip, intent, transactionStatus) -> None:
         self.checkout_url:str = checkout_url # shurjopay checkout url to redirect to payment page
-        self.amount:str = amount # payment amount
-        self.currency:str = currency # payment currency
-        self.sp_code:str = sp_code # shurjopay status code
-        self.sp_order_id:str = sp_order_id # shurjopay order id
-        self.customer_order_id:str = customer_order_id # marchent order id
-        self.customer_name:str = customer_name # customer name
-        self.customer_address:str = customer_address # customer address
-        self.customer_city:str = customer_city # customer city
-        self.customer_phone:str = customer_phone # customer phone
-        self.customer_email:str = customer_email # customer email
-        self.client_ip:str = client_ip # client ip
-        self.intent:str = intent # payment intent
-        self.transactionStatus:str = transactionStatus # payment transaction status
+        self.amount:str = amount
+        self.currency:str = currency 
+        self.sp_order_id:str = sp_order_id 
+        self.customer_order_id:str = customer_order_id 
+        self.customer_name:str = customer_name 
+        self.customer_address:str = customer_address 
+        self.customer_city:str = customer_city 
+        self.customer_phone:str = customer_phone
+        self.customer_email:str = customer_email 
+        self.client_ip:str = client_ip 
+        self.intent:str = intent 
+        self.transactionStatus:str = transactionStatus 
 
 
 class VerifiedPaymentDetailsModel(object):
@@ -61,26 +59,26 @@ class VerifiedPaymentDetailsModel(object):
         self.order_id:str = order_id # shurjopay order id
         self.currency:str = currency # payment currency
         self.amount:str = amount # payment amount
-        self.payable_amount:str = payable_amount # payment payable amount
-        self.recived_amount:str = recived_amount # payment received amount
-        self.discount_amount:str = discount_amount # payment discount amount
-        self.disc_percent:int = disc_percent # payment discount percent
-        self.usd_amt:int = usd_amt # payment usd amount
-        self.usd_rate:int = usd_rate # payment usd rate
-        self.card_holder_name:str = card_holder_name # card holder name
-        self.card_number:str = card_number # payment card number
-        self.phone_no :str= phone_no # customer phone number
-        self.bank_trx_id:str = bank_trx_id # bank transaction id
-        self.invoice_no:str = invoice_no # invoice number
-        self.bank_status:str = bank_status # bank status
-        self.customer_order_id:str = customer_order_id # marchent order id
-        self.sp_massage:str = sp_massage # shurjopay message
+        self.payable_amount:str = payable_amount 
+        self.recived_amount:str = recived_amount
+        self.discount_amount:str = discount_amount 
+        self.disc_percent:int = disc_percent 
+        self.usd_amt:int = usd_amt 
+        self.usd_rate:int = usd_rate 
+        self.card_holder_name:str = card_holder_name 
+        self.card_number:str = card_number 
+        self.phone_no :str= phone_no 
+        self.bank_trx_id:str = bank_trx_id
+        self.invoice_no:str = invoice_no 
+        self.bank_status:str = bank_status 
+        self.customer_order_id:str = customer_order_id 
+        self.sp_massage:str = sp_massage 
         self.sp_message:str=sp_message
-        self.sp_code:str = sp_code # shurjopay status code
-        self.name:str = name # customer name
-        self.email:str = email # customer email
-        self.address:str = address  # customer address
-        self.city:str = city # customer city
+        self.sp_code:str = sp_code 
+        self.name:str = name 
+        self.email:str = email
+        self.address:str = address  
+        self.city:str = city 
         '''
         Sometime customer have to send additional data like studentId 
 	    or any other information which have not any field given by shurjoPay.
@@ -90,6 +88,6 @@ class VerifiedPaymentDetailsModel(object):
         self.value2:str = value2
         self.value3:str = value3
         self.value4:str = value4
-        self.transaction_status:str = transaction_status # payment transaction status
-        self.method:str = method # payment method
-        self.date_time:str = date_time # payment date time
+        self.transaction_status:str = transaction_status 
+        self.method:str = method # payment method e.g.. bkash/rocket/nagad
+        self.date_time:str = date_time # payment timestamp
