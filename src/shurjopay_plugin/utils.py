57a4f7e0cce451914dbe6a2b2148a83ccce5fa8d
Mahabubul Hasan <mahabubul470@gmail.com>
2023-05-29 17:52:36 +0600

    v0.1.7: fix model bindings
    * Models are now instantiated using kwargs

--- src/shurjopay_plugin/utils.py
+++ src/shurjopay_plugin/utils.py
@@ -1,6 +1,7 @@
 from enum import Enum
 import socket
- 
+
+
 class Endpoints(str, Enum):
     '''This class contains all the endpoints of shurjoPayAPI'''
     API = 'api/'
@@ -12,13 +13,13 @@ class Endpoints(str, Enum):
 
 class CustomResponse:
     """This class is used to create custom response for shurjoPayAPI during payment process
-    
+
     Args:
     ----
     code(str/int): status code of the response
     message (str): message of the response
     """
-    
+
     def __init__(self, code, message):
         self.code = code
         self.message = message
@@ -26,57 +27,63 @@ class CustomResponse:
     def __str__(self):
         return f"HTTP Status code : {self.code} Message: {self.message}"
 
-    
+
 class ShurjopayStatus():
     '''This class contains the shurjoPayAPI Responses'''
-    AUTH_SUCCESS = CustomResponse(200, "Successfully authenticated with merchant")
+    AUTH_SUCCESS = CustomResponse(
+        200, "Successfully authenticated with Merchant")
     INVALID_ORDER_ID = CustomResponse('1011', "Invalid Payment ID")
     TRANSACTION_SUCCESS = CustomResponse('1000', "Transaction successful")
 
 
 class ShurjopayException(Exception):
     '''Exceptions during payment process 
-    
+
     Args:
     ---
         message: error message of different exception
         errors : errors of different exception
     '''
-    def __init__(self, message, errors):
-        super().__init__(message, errors) 
 
-class ShurjopayAuthException(ShurjopayException):
+    def __init__(self, message, errors):
+        super().__init__(message, errors)
+
+
+class ShurjopayAuthException(Exception):
     '''Exceptions during authentication process 
-    
+
     Args:
     ---
         message: authentication error message
         errors : authentication errors 
     '''
+
     def __init__(self, message, errors):
         super().__init__(message, errors)
 
+
 def get_client_ip():
-    '''This method is used to get the IP address of the merchant server
-    A socket connection is made to the IANA server and the IP address of the merchant server is returned  
-     
+    '''This method is used to get the IP address of the merchant server  
+
     Returns:
     -------
         ip_address (str): IP address of the merchant server 
-    
+
     Raises:
     ------
          ShurjoPayException while getting merchant IP address
+         ShurjoPayException while getting merchant IP address
     '''
-    
+
     IANA = '10.0.0.0'
-    PORT = 80
-    
+    PORT = 0
+
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect((IANA, PORT)) 
+        # connect() for UDP doesn't send packets
+        s.connect((IANA, PORT))
         return s.getsockname()[0]
     except ShurjopayException as ex:
-        raise ShurjopayException('Cannot connect to the internet"', ex) 
+        raise ShurjopayException('Cannot connect to the internet"', ex)
     finally:
         s.close()
